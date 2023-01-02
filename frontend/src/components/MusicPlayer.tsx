e020dce05f11ee5a08d61526a89a705c32c05e94
Adam Lisoń <konowaleczek71@gmail.com>
2023-01-02 18:46:58 +0100

    added player button delay

--- frontend/src/components/MusicPlayer.tsx
+++ frontend/src/components/MusicPlayer.tsx
@@ -1,5 +1,7 @@
 import {useEffect, useState} from "react";
 
+const BUTTON_DELAY = 2000
+
 export default function MusicPlayer({
     currentTrack,
     nextAction,
@@ -14,26 +16,47 @@ export default function MusicPlayer({
     resumeAction: any }
 ) {
     const [isPlaying, setIsPlaying] = useState(false);
+    const [delay, setDelay] = useState(false);
 
     // FIXME: Handle case when changing playback fails
     useEffect(() => {
         setIsPlaying(currentTrack?.is_playing)
     }, [currentTrack])
 
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setDelay(false)
+        }, BUTTON_DELAY)
+
+        return () => clearTimeout(timeout)
+    }, [delay])
+
     return (
         <div className={"music-player"}>
-            <button className={"player-button"} onClick={previousAction}><i className="bi bi-skip-start-fill"></i></button>
+            <button className={"player-button"} onClick={() => {
+                if(delay) return
+                setDelay(true)
+                previousAction();
+            }}><i className="bi bi-skip-start-fill"></i></button>
             { isPlaying ?
                 <button className={"player-button"} onClick={()=> {
+                    if(delay) return
+                    setDelay(true)
                     setIsPlaying(false)
                     pauseAction()
                 }}><i className="bi bi-pause-fill"></i></button> :
                 <button className={"player-button"} onClick={() => {
+                    if(delay) return
+                    setDelay(true)
                     setIsPlaying(true)
                     resumeAction()
                 }}><i className="bi bi-play-fill"></i></button>
             }
-            <button className={"player-button"} onClick={nextAction}><i className="bi bi-skip-end-fill"></i></button>
+            <button className={"player-button"} onClick={() => {
+                if(delay) return
+                setDelay(true)
+                nextAction()
+            }}><i className="bi bi-skip-end-fill"></i></button>
         </div>
     )
 }
